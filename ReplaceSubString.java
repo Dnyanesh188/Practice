52b40ad26ba3f62d03650a69e85fe7ee8676d4e4
Dnyanesh188 <127388699+Dnyanesh188@users.noreply.github.com>
2025-02-02 22:10:17 +0530

    Add files via upload

---
+++ ReplaceSubString.java
@@ -0,0 +1,7 @@
+public class ReplaceSubString{
+    public static void main(String[] args){
+        String str = "Hello world,";
+        String result = str.replace("world", "java");
+        System.out.println(result);
+    }
+}
